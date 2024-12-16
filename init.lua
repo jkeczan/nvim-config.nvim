2df2f4b48e6b1cd69b3d937f509ad833b6a33815
Jeremy Keczan <jeremykeczan@pillarinnovations.com>
2024-12-15 22:05:54 -0500

    latest updates

--- init.lua
+++ init.lua
@@ -618,6 +618,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
+        angularls = {},
         ts_ls = {},
         eslint = {
           capabilities = capabilities,
